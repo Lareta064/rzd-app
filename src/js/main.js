3b37f5d4e488a4fc4bf0b407861faa57c354eb3f
Lareta064 <lareta1@yandex.ru>
2024-11-17 17:29:27 +0300

    фикс

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,70 @@
 document.addEventListener("DOMContentLoaded", function (){
+	const  bodyEl = document.querySelector('body');
 	const buttonsBig = document.querySelectorAll('.btn-big');
 
 	if (buttonsBig.length > 0) {
-		buttonsBig.forEach((button, index) => {
+		buttonsBig.forEach((button) => {
 			button.addEventListener('click', () => {
-				buttonsBig.forEach((btn, i) => {
-					btn.classList.toggle('btn-press', i === index);
-				});
+				if (button.classList.contains('btn-press')) {
+					// Если кнопка уже активна, снимаем класс активности
+					button.classList.remove('btn-press');
+				} else {
+					// Убираем класс активности у всех кнопок
+					buttonsBig.forEach(btn => btn.classList.remove('btn-press'));
+					// Добавляем класс активности к текущей кнопке
+					button.classList.add('btn-press');
+				}
 			});
 		});
 	}
+/*[frame-btn] - modal-btn
+[frame-modal] - js-modal
+*/
+	/* =============== modal с атрибутом frame-modal ===============*/ 
+    const modalFramesOpen = document.querySelectorAll('[data-btn]');
+    const modalFrames = document.querySelectorAll('[data-modal]');
+    if( modalFrames.length > 0){
+      const modalFramesClose = document.querySelectorAll('[frame-close]');
+
+      for(let item of modalFramesOpen){
+        item.addEventListener('click', function(e){
+          for(let item of  modalFrames){
+            item.classList.remove('visible');
+            
+            bodyEl.classList.remove('lock');
+          }
+          e.preventDefault();
+          const itemAttr = item.getAttribute('data-btn');
+
+          for(let frame of modalFrames){
+            const frameAttr =frame.getAttribute('data-modal');	
+            if(frameAttr == itemAttr){
+              frame.classList.add('visible');
+              bodyEl.classList.add('lock');
+			 
+            }
+          }
+        });
+      }
+      /*==  закрыть модалки  frame-modal по клику на крестик ======*/
+      for(let item of modalFramesClose){
+        item.addEventListener('click', function(e){
+          e.preventDefault();
+          item.closest('[data-modal]').classList.remove('visible');
+          bodyEl.classList.remove('lock');
+		  
+		  
+        });
+      }
+      /*=============== закрыть модалки по клику вне ===============*/
+      for(let frame of modalFrames){
+        frame.addEventListener('click', function(e){
+          if(e.target === e.currentTarget){
+            this.classList.remove(`visible`);
+            bodyEl.classList.remove('lock');
+          }
+        });
+      }
+    }
+
 });
